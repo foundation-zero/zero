f1427edae042ef658b32ef032eb886316bf40f4f
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2025-02-13 13:01:03 +0100

    Add thruster controls (#3)

--- zero-thrs-control/simulation/io/thruster.py
+++ zero-thrs-control/simulation/io/thruster.py
@@ -1,29 +1,45 @@
 from typing import Annotated
 from simulation.io.base import ThrsModel, Meta
-from simulation.io.sensor import (
-    FlowSensor,
-    PressureSensor,
-    Pump,
-    TemperatureSensor,
-    Valve,
-)
+import simulation.io.sensor as sensor
+import simulation.io.control as control
 
 
 class ThrustersSensors(ThrsModel):
-    thruster_pump_1: Annotated[Pump, Meta("5001015")]
-    thruster_pump_2: Annotated[Pump, Meta("5001016")]
-    thrusters_temperature_aft_return: Annotated[TemperatureSensor, Meta("5001038-01")]
-    thrusters_temperature_fwd_return: Annotated[TemperatureSensor, Meta("5001038-02")]
-    thrusters_temperature_supply: Annotated[TemperatureSensor, Meta("5001038-28")]
-    thrusters_temperature_fwd_supply: Annotated[TemperatureSensor, Meta("5001038-29")]
-    thrusters_temperature_aft_supply: Annotated[TemperatureSensor, Meta("5001038-30")]
-    thrusters_mix_aft: Annotated[Valve, Meta("5001042-01")]
-    thrusters_mix_fwd: Annotated[Valve, Meta("5001042-02")]
-    thrusters_mix_exchanger: Annotated[Valve, Meta("5001045-03")]
-    thrusters_flow_fwd: Annotated[FlowSensor, Meta("50001057-22")]
-    thrusters_flow_aft: Annotated[FlowSensor, Meta("50001057-23")]
-    thrusters_flowcontrol_aft: Annotated[Valve, Meta("5001064-01")]
-    thrusters_flowcontrol_fwd: Annotated[Valve, Meta("5001064-02")]
-    thrusters_shutoff_recovery: Annotated[Valve, Meta("5001069-10")]
-    thrusters_pressure_recovery: Annotated[PressureSensor, Meta("5001097-01")]
-    thrusters_pressure_cooling: Annotated[PressureSensor, Meta("5001097-02")]
+    thruster_pump_1: Annotated[sensor.Pump, Meta("5001015")]
+    thruster_pump_2: Annotated[sensor.Pump, Meta("5001016")]
+    thrusters_temperature_aft_return: Annotated[
+        sensor.TemperatureSensor, Meta("5001038-01")
+    ]
+    thrusters_temperature_fwd_return: Annotated[
+        sensor.TemperatureSensor, Meta("5001038-02")
+    ]
+    thrusters_temperature_supply: Annotated[
+        sensor.TemperatureSensor, Meta("5001038-28")
+    ]
+    thrusters_temperature_fwd_supply: Annotated[
+        sensor.TemperatureSensor, Meta("5001038-29")
+    ]
+    thrusters_temperature_aft_supply: Annotated[
+        sensor.TemperatureSensor, Meta("5001038-30")
+    ]
+    thrusters_mix_aft: Annotated[sensor.Valve, Meta("5001042-01")]
+    thrusters_mix_fwd: Annotated[sensor.Valve, Meta("5001042-02")]
+    thrusters_mix_exchanger: Annotated[sensor.Valve, Meta("5001045-03")]
+    thrusters_flow_fwd: Annotated[sensor.FlowSensor, Meta("50001057-22")]
+    thrusters_flow_aft: Annotated[sensor.FlowSensor, Meta("50001057-23")]
+    thrusters_flowcontrol_aft: Annotated[sensor.Valve, Meta("5001064-01")]
+    thrusters_flowcontrol_fwd: Annotated[sensor.Valve, Meta("5001064-02")]
+    thrusters_shutoff_recovery: Annotated[sensor.Valve, Meta("5001069-10")]
+    thrusters_pressure_recovery: Annotated[sensor.PressureSensor, Meta("5001097-01")]
+    thrusters_pressure_cooling: Annotated[sensor.PressureSensor, Meta("5001097-02")]
+
+
+class ThrustersControls(ThrsModel):
+    thruster_pump_1: Annotated[control.Pump, Meta("5001015")]
+    thruster_pump_2: Annotated[control.Pump, Meta("5001016")]
+    thrusters_mix_aft: Annotated[control.Valve, Meta("5001042-01")]
+    thrusters_mix_fwd: Annotated[control.Valve, Meta("5001042-02")]
+    thrusters_mix_exchanger: Annotated[control.Valve, Meta("5001045-03")]
+    thrusters_flowcontrol_aft: Annotated[control.Valve, Meta("5001064-01")]
+    thrusters_flowcontrol_fwd: Annotated[control.Valve, Meta("5001064-02")]
+    thrusters_shutoff_recovery: Annotated[control.Valve, Meta("5001069-10")]
