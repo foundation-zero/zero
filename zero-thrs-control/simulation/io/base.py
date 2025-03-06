aa72c1bc857b7723d18f6b053b79ccbf2ffd39db
Alje <alje.vandam@foundationzero.org>
2025-03-06 08:53:06 +0100

    Only use alias for serialization

--- zero-thrs-control/simulation/io/base.py
+++ zero-thrs-control/simulation/io/base.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import AliasGenerator, BaseModel, ConfigDict
 from simulation.utils.string import hyphenize
 
 
 class ThrsModel(BaseModel):
     """ThrsModel provides the conversion between the dashes in MQTT to Python underscores"""
 
-    class Config:
-        alias_generator = hyphenize
+    model_config = ConfigDict(
+        alias_generator=AliasGenerator(
+            serialization_alias=hyphenize,
+        )
+    )
 
 
 class Stamped[T](ThrsModel):
