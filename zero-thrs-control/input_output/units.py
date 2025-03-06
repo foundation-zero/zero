7d15728aa546a1bffee865f4f067119780dbfa1d
Alje <alje.vandam@foundationzero.org>
2025-03-06 12:31:33 +0100

    Constrain units

--- zero-thrs-control/input_output/units.py
+++ zero-thrs-control/input_output/units.py
@@ -1,6 +1,10 @@
 # Per https://docs.google.com/document/d/11EGlLqZ21uHy4ICmhvPx9uKOwm0guKgWxY6-1zSQ2mQ/edit?tab=t.0#heading=h.l7ph84h61wda
-type Celsius = float
-type LMin = float
-type Hz = float
-type Ratio = float
-type Bar = float
+from typing import Annotated
+
+from pydantic import Field
+
+type Celsius = Annotated[float, Field(ge=-273.15)]
+type LMin = Annotated[float, Field(ge=0)]
+type Hz = Annotated[float, Field(ge=0)]
+type Ratio = Annotated[float, Field(ge=0, le=1)]
+type Bar = Annotated[float, Field(ge=0)]
