a0e51a887f9ffd7701c87c2af58514d1d2041fe3
Alje <alje.vandam@foundationzero.org>
2025-04-01 16:15:57 +0200

    Add PVT module and io test

---
+++ zero-thrs-control/src/input_output/modules/PVT.py
@@ -0,0 +1,14 @@
+from input_output.base import ThrsModel
+
+
+class PvtSensorValues(ThrsModel):
+    pass 
+
+class PvtControlValues(ThrsModel):
+    pass
+
+class PvtSimulationInputs(ThrsModel):
+    pass
+
+class PvtSimulationOutputs(ThrsModel):
+    pass
