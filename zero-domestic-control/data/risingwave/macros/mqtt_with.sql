59ae1db496675ae905a34efa0ec328c764509cb0
Dustin van Weersel <dustin.vanweersel@foundationzero.org>
2025-07-01 11:16:24 +0200

    Cleanup env for pushing to singels

--- zero-domestic-control/data/risingwave/macros/mqtt_with.sql
+++ zero-domestic-control/data/risingwave/macros/mqtt_with.sql
@@ -1,6 +1,6 @@
 {% macro mqtt_with(topic) %}WITH (
   connector = 'mqtt',
-  url       = 'tcp://{{ env_var('MQTT_HOST') }}:{{ env_var('MQTT_PORT') }}',
+  url       = '{{ env_var('MQTT_PROTOCOL') }}://{{ env_var('MQTT_HOST') }}:{{ env_var('MQTT_PORT') }}',
   topic     = '{{ topic }}',
   qos       = 'exactly_once',
 ) FORMAT PLAIN ENCODE JSON;
--- zero-domestic-control/data/risingwave/macros/sink_append_pg.sql
+++ zero-domestic-control/data/risingwave/macros/sink_append_pg.sql
@@ -1,6 +1,6 @@
 {% macro sink_append_pg(table) %}WITH (
   connector = 'jdbc',
-  jdbc.url = 'jdbc:postgresql://postgres:{{ env_var('PG_PORT') }}/{{ env_var('PG_DB') }}?user={{ env_var('PG_USER') }}&password={{ env_var('PG_PASSWORD') }}',
+  jdbc.url = 'jdbc:postgresql://{{ env_var('PG_HOST') }}:{{ env_var('PG_PORT') }}/{{ env_var('PG_DB') }}?user={{ env_var('PG_USER') }}&password={{ env_var('PG_PASSWORD') }}',
   table.name = '{{ table}}',
   force_append_only='true',
   type = 'append-only'
