fdb7d68ee1e86e0fbc82eabdf6fd45109ea92fd6
Dustin van Weersel <dustin.vanweersel@foundationzero.org>
2025-07-29 11:07:31 +0200

    Fix pytest

--- zero-data/tests/io_processing/generator/test_io_metadata.py
+++ zero-data/tests/io_processing/generator/test_io_metadata.py
@@ -6,9 +6,13 @@ from zero_data.dbt_gen.io_metadata import IOMetadataWriter
 
 def test_io_metadata(marpower_io_list):
     with patch("polars.dataframe.DataFrame.write_csv", create=True) as mock_write_csv:
-        io_metadata_df = IOMetadataWriter(Path("/not-used")).write_io_metadata_csv(
-            marpower_io_list, "marpower"
-        )
+        with (
+            patch("pathlib.Path.mkdir"),
+            patch("pathlib.Path.is_dir", return_value=True),
+        ):
+            io_metadata_df = IOMetadataWriter(Path("/not-used")).write_io_metadata_csv(
+                marpower_io_list, "marpower"
+            )
         mock_write_csv.assert_called_with(
             PosixPath("/not-used/seeds/io_metadata_marpower.csv"),
             quote_style="non_numeric",
