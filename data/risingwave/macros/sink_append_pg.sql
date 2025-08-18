19d6444872e8f9d6f71737fdad26c69d5848a2b7
Dustin van Weersel <dustin.vanweersel@foundationzero.org>
2025-08-18 12:39:23 +0200

    Bugfix CI

--- data/risingwave/macros/sink_append_pg.sql
+++ data/risingwave/macros/sink_append_pg.sql
@@ -1,6 +1,6 @@
 {% macro sink_append_pg(table, database) %}WITH (
   connector = 'jdbc',
-  jdbc.url = 'jdbc:postgresql://{{ env_var('PG_HOST') }}:{{ env_var('PG_PORT') }}/{{ database }}?user={{ env_var('PG_USER') }}&password={{ env_var('PG_PASSWORD') }}',
+  jdbc.url = 'jdbc:postgresql://{{ env_var('PG_HOST_DOCKER') }}:{{ env_var('PG_PORT') }}/{{ database }}?user={{ env_var('PG_USER') }}&password={{ env_var('PG_PASSWORD') }}',
   table.name = '{{ table}}',
   force_append_only='true',
   type = 'append-only'
