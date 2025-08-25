9a3fc995d47427c1d82f756fe7c0e6d1cbbfeed5
Alje <alje.vandam@foundationzero.org>
2025-08-25 10:37:25 +0200

    Update modules

--- zero-thrs-control/src/simulation/models/fmu_paths.py
+++ zero-thrs-control/src/simulation/models/fmu_paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-thrusters_path = str(Path(__file__).resolve().parent / "thruster_moduleV14.fmu")
-pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV5.fmu")
+thrusters_path = str(Path(__file__).resolve().parent / "thruster_moduleV15.fmu")
+pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV8.fmu")
 consumers_path = str(Path(__file__).resolve().parent / "consumers_moduleV4.fmu")
-pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV5.fmu")
+pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV6.fmu")
 high_temperature_path = str(Path(__file__).resolve().parent / "HTSystem_moduleV6.fmu")
