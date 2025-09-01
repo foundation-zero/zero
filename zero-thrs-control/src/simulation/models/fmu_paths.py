6986d93e8fc26d551e6e91ec136f4dd262f2186e
Alje <alje.vandam@foundationzero.org>
2025-09-01 09:13:52 +0200

    Update to PVT module V11

--- zero-thrs-control/src/simulation/models/fmu_paths.py
+++ zero-thrs-control/src/simulation/models/fmu_paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 thrusters_path = str(Path(__file__).resolve().parent / "thruster_moduleV16.fmu")
-pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV9.fmu")
+pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV11.fmu")
 consumers_path = str(Path(__file__).resolve().parent / "consumers_moduleV4.fmu")
 pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV7.fmu")
 high_temperature_path = str(Path(__file__).resolve().parent / "HTSystem_moduleV6.fmu")
