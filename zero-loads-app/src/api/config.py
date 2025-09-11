9da94bab81f2fd8727810d5b02d71bb111d46a22
Dustin van Weersel <dustin.vanweersel@foundationzero.org>
2025-09-11 09:20:49 +0200

    Boilerplate API

--- zero-loads-app/src/api/config.py
+++ zero-loads-app/src/api/config.py
@@ -5,7 +5,12 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="allow"
     )
-    mqtt_host: str
-    mqtt_port: int
+    pg_host: str
+    pg_port: str
+    pg_user: str
+    pg_password: str
+    pg_db: str
 
-    jwt_secret: str
+    @property
+    def pg_url(self) -> str:
+        return f"postgresql://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_db}"
