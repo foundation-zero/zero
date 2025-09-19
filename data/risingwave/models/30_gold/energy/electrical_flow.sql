3789a19e1368cdb43c104a180b131821eabb79c1
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-09-19 09:18:54 +0200

    New electrical flow calculation
    For the electrical flow (Prius component) the current electrical energy consumption is calculated by calculating a running average over the last 5 minutes per topic. Then the topics are grouped by group name and the energy values are ageraged again for that group.

--- data/risingwave/models/30_gold/energy/electrical_flow.sql
+++ data/risingwave/models/30_gold/energy/electrical_flow.sql
@@ -1,23 +1,19 @@
 {{ config(materialized='materialized_view') }}
 SELECT
-    MAX(time_stamp) AS power_timestamp,
-    consumer_group AS group_name,
-    SUM(active_power) AS power,
+    MAX(timestamp) AS timestamp,
+    group_name,
+    AVG(avg_power) AS avg_group_power,
     NULL as power_stored                    /* Used for battery data only */
 FROM
 (
 	SELECT
-        ROW_NUMBER()
-        	OVER (PARTITION BY electrical_consumption.topic
-	        ORDER BY GREATEST(electrical_consumption.active_power_timestamp, electrical_consumption.power_factor_timestamp) DESC) AS rn,
-        electrical_consumption.topic,
-        electrical_consumption.consumer_group,
-        electrical_consumption.active_power,
-        electrical_consumption.power_factor,
-        electrical_consumption.active_power_timestamp AS time_stamp
-	FROM {{ ref('electrical_consumption') }} AS electrical_consumption
-	WHERE electrical_consumption.consumer_group IS NOT NULL
+		MAX(electrical_consumption.active_power_timestamp) AS timestamp,
+		electrical_consumption.consumer_group AS group_name,
+		AVG(electrical_consumption.active_power) AS avg_power
+	FROM public.electrical_consumption AS electrical_consumption
+	WHERE
+		electrical_consumption.consumer_group IS NOT NULL
+		AND electrical_consumption.active_power_timestamp > NOW() - INTERVAL '5 minutes'
+	GROUP BY topic, group_name
 )
-WHERE rn = 1
 GROUP BY group_name
-ORDER BY group_name
