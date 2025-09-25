271593777d38ffcc15968a89e1a417345b4baf7f
David Hoevers <david@manklavier.nl>
2025-09-25 21:53:32 +0200

    ZERO-847: implement dropdown

--- zero-ui/src/components/ui/shadcn/button/index.ts
+++ zero-ui/src/components/ui/shadcn/button/index.ts
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 export { default as Button } from "./Button.vue";
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 cursor-pointer whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 cursor-pointer whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive duration-250",
   {
     variants: {
       variant: {
@@ -13,6 +13,7 @@ export const buttonVariants = cva(
           "bg-muted border-border-subtle border text-muted-foreground shadow-xs hover:bg-border-subtle hover:text-foreground active:bg-background active:text-muted-foreground disabled:bg-disabled disabled:text-disabled-foreground",
         ghost:
           "text-muted-foreground hover:bg-border-subtle hover:text-foreground active:bg-background active:text-muted-foreground disabled:text-disabled-foreground",
+        link: "text-brand-dull !px-0 disabled:text-brand-muted underline underline-offset-4 hover:text-brand active:text-brand-dull/80",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
