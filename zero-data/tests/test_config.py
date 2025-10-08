35517f72615927986d9ff7ef40f1e880ce8de470
Floris Smit <florissmit10@gmail.com>
2025-10-08 11:30:26 +0200

    Fix test missing computed value

--- zero-data/tests/test_config.py
+++ zero-data/tests/test_config.py
@@ -9,4 +9,4 @@ from zero_data.config import MQTTConfig
 )
 def test_mqtt_config_not_default():
     config = MQTTConfig()  # pyright: ignore
-    assert config.model_dump() == {"host": "example.com", "port": 1884}
+    assert config.model_dump() == {"host": "example.com", "port": 1884, "uri": "example.com:1884"}
