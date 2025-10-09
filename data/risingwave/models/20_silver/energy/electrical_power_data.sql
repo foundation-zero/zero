85fb12f41d33b453025a4cedddc6dec87eef8936
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2025-10-09 09:08:58 +0100

    Feature/zero 644 silver layer producers (#63)
    * Electrical producers: PVT
    
    Added PVT as electrical producer by adding placeholder data rows in Marpower IO list, a new table in raw layer and new materialized views in bronze and silver layer.
    
    * Renamed power_tag_metadata.csv
    
    Renamed file to electrical_energy_metadata, because it contains metadata for more than just the power tags.
    
    * Add shore power and thrusters
    
    Added shore power and hydrogeneration data points in marpower IO list. Added tables and materialized views. Updated materialized view in silver layer to combine pvt, shore power and hydrogeneration.
    
    * Added missing LF at EOF
    
    * Removed spaces
    
    * Fixed typo in metadata
    
    * Rename consumer_group to producer_group
    
    * Added macro for power updates
    
    * Renamed marpower IO list
    
    * Update zero-data/src/zero_data/config.py
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
    
    * Renamed Marpower IO list
    
    * Updated power_update query
    
    Update query that updates power values. This now combines multiple tables with power data. As a result, fewer bronze and silver layer tables/viewz are needed.
    
    * Updated power_update query
    
    And therefor no longer need macro.
    
    * Fixed bug after merging with main.
    
    * Update power_updates.sql
    
    * Renamed alias in power_updates query
    
    ---------
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>

--- data/risingwave/models/20_silver/energy/electrical_power_data.sql
+++ data/risingwave/models/20_silver/energy/electrical_power_data.sql
@@ -5,7 +5,7 @@ SELECT
   power_updates.power_factor AS power_factor,
   power_updates.power_factor_timestamp AS power_factor_timestamp,
   power_updates.topic AS topic,
-  consumers.electrical_system AS electrical_system,
-  consumers.group AS consumer_group
-FROM {{ ref('power_tag_power_updates') }} AS power_updates
-JOIN {{ ref('power_tag_metadata') }} AS consumers ON power_updates.topic = consumers.preferred_mqtt_topic
+  metadata.electrical_system AS electrical_system,
+  metadata.group AS consumer_group
+FROM {{ ref('power_updates') }} AS power_updates
+JOIN {{ ref('electrical_energy_metadata') }} AS metadata ON power_updates.topic = metadata.preferred_mqtt_topic
