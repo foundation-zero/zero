86d1bf9497b4e1bd38fd49a9bda483579ab6d705
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2025-10-23 13:08:42 +0200

    Update data/risingwave/models/30_gold/energy/electrical_soc.sql
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>

--- data/risingwave/models/30_gold/energy/electrical_soc.sql
+++ data/risingwave/models/30_gold/energy/electrical_soc.sql
@@ -3,6 +3,6 @@ SELECT
 	LAST_VALUE(time ORDER BY time) AS time,
 	topic,
 	group_name,
-	LAST_VALUE(stored_energy ORDER BY time) stored_energy
+	LAST_VALUE(stored_energy ORDER BY time) AS stored_energy
 FROM {{ ref('bms_master_power_data') }}
 GROUP BY topic, group_name
