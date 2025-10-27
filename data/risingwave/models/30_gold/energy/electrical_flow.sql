4d1a2df820e5e28cb3b86457348440bd18d48838
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2025-10-27 07:26:27 +0100

    Batterij data gold layer (#76)
    * Added soc
    
    * Add charging/discharging data to electrical flow
    
    * Renamed timestamp
    
    * Changed SoC query in gold layer
    
    * Minor formatting updates
    
    * Update data/risingwave/models/30_gold/energy/electrical_flow.sql
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
    
    * Update data/risingwave/models/30_gold/energy/electrical_soc.sql
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
    
    * Update data/risingwave/models/30_gold/energy/electrical_soc.sql
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
    
    ---------
    
    Co-authored-by: Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>

--- data/risingwave/models/30_gold/energy/electrical_flow.sql
+++ data/risingwave/models/30_gold/energy/electrical_flow.sql
@@ -1,19 +1,32 @@
 {{ config(materialized='materialized_view') }}
 SELECT
-    MAX(timestamp) AS timestamp,
+    MAX(time) AS time,
     group_name,
-    SUM(avg_power) AS avg_group_power,
-    NULL as power_stored                    /* Used for battery data only */
+    SUM(avg_power) AS group_power
 FROM
 (
+	-- Consumer and producer power data
 	SELECT
-		MAX(electrical_power_data.active_power_timestamp) AS timestamp,
+		MAX(electrical_power_data.time) AS time,
 		electrical_power_data.group_name AS group_name,
 		AVG(electrical_power_data.active_power) AS avg_power
 	FROM {{ ref('consumer_producer_power_data') }} AS electrical_power_data
 	WHERE
 		electrical_power_data.group_name IS NOT NULL
-		AND electrical_power_data.active_power_timestamp > NOW() - INTERVAL '5 minutes'
+		AND electrical_power_data.time > NOW() - INTERVAL '5 minutes'
+	GROUP BY topic, group_name
+	
+	UNION ALL
+	
+	-- Charging / discharging power data
+	SELECT
+		MAX(electrical_charging_data.time) AS time,
+		electrical_charging_data.group_name AS group_name,
+		AVG(electrical_charging_data.voltage_a) * AVG(electrical_charging_data.current_a) AS avg_power
+	FROM {{ ref('battery_dc_converter_power_data') }} AS electrical_charging_data
+	WHERE
+		electrical_charging_data.group_name IS NOT NULL
+		AND electrical_charging_data.time > NOW() - INTERVAL '5 minutes'
 	GROUP BY topic, group_name
 )
 GROUP BY group_name
