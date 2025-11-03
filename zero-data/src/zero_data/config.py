4841f29d984fc0583b74cd861301b9ee39f4e962
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-11-03 07:35:08 +0100

    Moved mocked list to the top

--- zero-data/src/zero_data/config.py
+++ zero-data/src/zero_data/config.py
@@ -7,9 +7,9 @@ from zero_data.io_list.types import Source
 
 io_lists: List[Tuple[Source, List[str]]] = [
     ("marpower", [
+        "ZERO mocked IO-List.xlsx",
         "52422003_3210_AMCS IO-List R2.14.xlsx",
-        "52422003_3211_PMS IO-List R2.6.xlsx",
-        "ZERO mocked IO-List.xlsx"
+        "52422003_3211_PMS IO-List R2.6.xlsx"
     ])
 ]
 
