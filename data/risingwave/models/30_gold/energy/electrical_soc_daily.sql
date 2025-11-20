d9abf81e21c150ba600b834c9810a3d9e0c7275d
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-11-20 11:27:06 +0100

    State of Charge (SoC) data per hour
    Added materialized view to hold SoC values per topic, per hour.
    Removed materialized view that contained SoC values per day.
    
    => SoC historical data uses dummy data for testing and visualization purposes, this dummy data will be removed soon.

--- data/risingwave/models/30_gold/energy/electrical_soc_daily.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='materialized_view') }}
-
--- For now dummy data is loaded in the materialized view. This will be replace by actual measured data.
-SELECT * FROM (
-    VALUES
-    ('a', ('2025-11-11')::date, 91.3),
-    ('b', ('2025-11-11')::date, 141.6)
-) AS data(topic, day, soc)
