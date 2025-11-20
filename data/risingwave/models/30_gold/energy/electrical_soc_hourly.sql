6e0d8553c5cf4e290204ec43c0781fb73111e26b
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-11-20 12:45:27 +0100

    SoC historical data
    Replaced dummy data with data from silver layer for SoC. Stored energy is calculated as an average value per minute, and then converted to an average value per hour (Wh).

--- data/risingwave/models/30_gold/energy/electrical_soc_hourly.sql
+++ data/risingwave/models/30_gold/energy/electrical_soc_hourly.sql
@@ -1,4 +1,39 @@
 {{ config(materialized='materialized_view') }}
 
--- This temporarily uses dummy data for testing and visualization purposes. This model will be updated to process data from the silver layer.
- SELECT * FROM {{ ref('electrical_soc_dummy_data')}}
+-- Calculate stored energy (Wh) by calculating average values per minute to determine which minutes have data.
+-- The energy value is then summed up and divided by 60 to go from Watt minutes to Watt hours.
+WITH per_minute AS (
+    SELECT
+        soc_data.electrical_system AS electrical_system,
+        soc_data.group_name AS group_name,
+        soc_data.sub_group_name AS sub_group_name,
+        soc_data.topic AS topic,
+        window_start AS min_start,
+        AVG(soc_data.stored_energy) AS avg_soc
+    FROM
+    TUMBLE (
+        {{ ref('bms_master_power_data') }},
+        stored_energy_timestamp,
+        INTERVAL '1 MINUTE'
+    ) AS soc_data
+    GROUP BY
+        electrical_system,
+        group_name,
+        sub_group_name,
+        topic,
+        window_start
+)
+SELECT
+    electrical_system,
+    group_name,
+    sub_group_name,
+    topic,
+    date_trunc('hour', min_start) AS hour,
+    SUM(avg_soc) / 60.0 AS soc
+FROM per_minute
+GROUP BY
+    electrical_system,
+    group_name,
+    sub_group_name,
+    topic,
+    date_trunc('hour', min_start)
