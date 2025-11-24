7b697e799ba44ce585317c42cb0b00dacd741791
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-11-24 10:30:03 +0100

    Update electrical_soc_dummy_data.sql

--- data/risingwave/models/30_gold/energy/electrical_soc_dummy_data.sql
+++ data/risingwave/models/30_gold/energy/electrical_soc_dummy_data.sql
@@ -1,20 +1,19 @@
-{{ config(materialized='table_with_connector') }}
+{{ config(materialized='table') }}
 
 -- This temporarily table is filled with dummy data which is to enable testing and visualization historical SoC data. This table will be removed soon.
-CREATE TABLE {{ this }} AS
 SELECT
     topic,
     hour,
-    soc_value - 30 * random() * GREATEST(0, date_part('day', hour - (now() - INTERVAL '4 days'))) AS soc
+    soc_value - 35 * random() * GREATEST(0, date_part('day', hour - (now() - INTERVAL '4 days'))) AS soc
 FROM (
     SELECT
         e.entity AS topic,
         g.ts AS hour,
-        e.base::numeric * (1 + (random() - 0.5) * 0.8) AS soc_value
+        e.base::numeric * (1 + (random() - 0.5) * 0.9) AS soc_value
     FROM (
-    VALUES
-        ('a', 100.0),
-        ('b', 150.0)
+        VALUES
+            ('a', 100.0),
+            ('b', 150.0)
     ) AS e(entity, base)
     CROSS JOIN generate_series(
         date_trunc('hour', now() - interval '60 days')::timestamptz,
