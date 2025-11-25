52db40af795f8abf4f4cd0d4e6d6b6c1d62d8393
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2025-11-25 07:45:03 +0100

    Feature/SoC history gold layer (#114)
    * State of Charge (SoC) data per hour
    
    Added materialized view to hold SoC values per topic, per hour.
    Removed materialized view that contained SoC values per day.
    
    => SoC historical data uses dummy data for testing and visualization purposes, this dummy data will be removed soon.
    
    * Update energy.yml
    
    * SoC historical data
    
    Replaced dummy data with data from silver layer for SoC. Stored energy is calculated as an average value per minute, and then converted to an average value per hour (Wh).
    
    * SoC historical data
    
    Replaced dummy data with data from silver layer for SoC. Stored energy is calculated as an average value per minute, and then converted to an average value per hour (Wh).
    
    * Delete electrical_soc_dummy_data.sql
    
    * Simplyfied hourly soc query
    
    * Minor fixes after review

--- data/risingwave/models/30_gold/energy/electrical_soc_hourly.sql
+++ data/risingwave/models/30_gold/energy/electrical_soc_hourly.sql
@@ -1,4 +1,22 @@
 {{ config(materialized='materialized_view') }}
 
--- This temporarily uses dummy data for testing and visualization purposes. This model will be updated to process data from the silver layer.
- SELECT * FROM {{ ref('electrical_soc_dummy_data')}}
+-- Calculate stored energy (Wh) by calculating average values per hour per topic.
+SELECT
+    soc_data.electrical_system AS electrical_system,
+    soc_data.group_name AS group_name,
+    soc_data.sub_group_name AS sub_group_name,
+    soc_data.topic AS topic,
+    window_start AS hour,
+    AVG(soc_data.stored_energy) AS soc
+FROM
+TUMBLE(
+    {{ ref('bms_master_power_data') }},
+    stored_energy_timestamp,
+    INTERVAL '1 HOUR'
+) AS soc_data
+GROUP BY
+    electrical_system,
+    group_name,
+    sub_group_name,
+    topic,
+    window_start
