7fc9857295d24ca8e6aeb86a5e2e0b114a7f710e
Alje <alje.vandam@foundationzero.org>
2025-11-26 14:51:26 +0100

    --wip-- [skip ci]

--- zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
+++ zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 thrusters_path = str(Path(__file__).resolve().parent / "thruster_moduleV18.fmu")
-pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV13_1.fmu")
+pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV14.fmu")
 consumers_path = str(Path(__file__).resolve().parent / "consumers_moduleV7_1.fmu")
 pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV7.fmu")
-high_temperature_path = str(Path(__file__).resolve().parent / "HT_systemV10_1.fmu")
+high_temperature_path = str(Path(__file__).resolve().parent / "HT_systemV11.fmu")
