69c3a4aeb29da11beb86fc9dc0660f795bce0639
aljevandam <19215316+aljevandam@users.noreply.github.com>
2025-12-05 15:27:11 +0100

    Fix HT test (#134)

--- zero-thrs-control/tests/modules/high_temperature/test_io.py
+++ zero-thrs-control/tests/modules/high_temperature/test_io.py
@@ -1,4 +1,4 @@
-import pytest
+import re
 from thrs.input_output.modules.high_temperature import (
     HighTemperatureControlValues,
     HighTemperatureSensorValues,
@@ -9,7 +9,6 @@ from tests.modules.conftest import compare_fmu_to_class, compare_yard_tags
 from thrs.simulation.models.fmu_paths import high_temperature_path
 
 
-@pytest.mark.skip(reason="Need XRG to remove superfluous output variables from FMU")
 def test_high_temperature_fmu_names():
     missing_in_py, missing_in_fmu = compare_fmu_to_class(
         high_temperature_path,
@@ -18,8 +17,12 @@ def test_high_temperature_fmu_names():
         HighTemperatureSimulationInputs.zero(),
         HighTemperatureSimulationOutputs.zero(),
     )
-
-    assert not missing_in_py, f"Missing in Python: {missing_in_py}"
+    missing_in_py_excluding_superfluous_outputs = {
+        item for item in missing_in_py if not re.match(r"^z_.*\.", item)
+    }  # Remove superfluous outputs contained in the High Temperature FMU due to the way it's constructed (combining submodules)
+    assert not missing_in_py_excluding_superfluous_outputs, (
+        f"Missing in Python: {missing_in_py_excluding_superfluous_outputs}"
+    )
     assert not missing_in_fmu, f"Missing in FMU: {missing_in_fmu}"
 
 
