1011e2b48e7be1327f6e69e2b88720dd43192d34
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-12-05 09:07:15 +0100

    Updated dbt models and seeds
    - Removed unused models (generation not working correctly).
    - Moved yml files for documentation out of models folders, but in its parent folder.
    - Removed duplicate powertag entries in mocked IO list.

--- data/risingwave/models/00_source/marpower/450000_firedetection.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table_with_connector') }}
-CREATE TABLE {{ this }} (
-	time TIMESTAMPTZ AS proctime(),
-	"FIRE_DETECT_FAULT"	{{ marpower_struct("BOOLEAN") }},
-)
-INCLUDE partition AS topic
-{{ mqtt_with('marpower/450000-firedetection') }}
