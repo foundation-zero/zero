d698d562435c32e0482555989145f4783a06c3de
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2025-12-08 10:01:58 +0100

    Feature/Update io list handling and dbt models (#133)
    * Added PMS R2.7 and updated tests
    
    * Added powertags to mock list
    
    * Updated dbt models and seeds
    
    - Removed unused models (generation not working correctly).
    - Moved yml files for documentation out of models folders, but in its parent folder.
    - Removed duplicate powertag entries in mocked IO list.
    
    * Fixed failing test

--- data/risingwave/models/00_source/marpower/170000_steering_system.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table_with_connector') }}
-CREATE TABLE {{ this }} (
-	time TIMESTAMPTZ AS proctime(),
-	"STEER_FREQ_DR_FAIL"	{{ marpower_struct("BOOLEAN") }},
-)
-INCLUDE partition AS topic
-{{ mqtt_with('marpower/170000-steering-system') }}
