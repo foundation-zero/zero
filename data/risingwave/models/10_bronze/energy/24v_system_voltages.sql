b29d238f6d98ddc47a401f3981d4e614d9352084
Stefan Hofman <stefan.hofman@foundationzero.org>
2025-12-19 08:22:22 +0100

    Added bronze layer
    - Materialized view to track voltage measurements for each 24v system
    - Updates view to contain updated values of current of each 24v device and the related voltage of the right 24v system

---
+++ data/risingwave/models/10_bronze/energy/24v_system_voltages.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='materialized_view') }}
+
+SELECT time, 'e1' AS system_code, "GEN_SERVICE_14E1_VOLTAGE" AS "voltage"
+FROM {{ ref('450000_24vdc_system_general')}}
+UNION ALL
+SELECT time, 'e2' AS system_code, "EMERG_14E2_VOLTAGE" AS "voltage"
+FROM {{ ref('450000_24vdc_system_general')}}
+UNION ALL 
+SELECT time, 'e3' AS system_code, "GMDSS_14E3_VOLTAGE" AS "voltage"
+FROM {{ ref('450000_24vdc_system_general')}}
