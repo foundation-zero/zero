0b02669b12e1436cf76777ca1dee27578674381a
Stefan Hofman <stefan.hofman@foundationzero.org>
2026-01-05 11:09:52 +0100

    Update test_marpower.py

--- zero-data/tests/io_list/test_marpower.py
+++ zero-data/tests/io_list/test_marpower.py
@@ -56,4 +56,4 @@ def test_mocked_io_excel():
     )
     assert marpower_io_result.io_list.shape == (430, 13)
     assert marpower_io_result.io_list.columns == expected_io_columns
-    assert len(marpower_io_result.topics) == 213
+    assert len(marpower_io_result.topics) == 214
