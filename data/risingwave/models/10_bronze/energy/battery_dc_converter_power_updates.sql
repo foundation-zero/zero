0c55c2b5305c2c8d10d6fa66a347243195069805
Stefan Hofman <stefan.hofman@foundationzero.org>
2026-01-06 11:13:41 +0100

    Minor changes in naming

--- data/risingwave/models/10_bronze/energy/battery_dc_converter_power_updates.sql
+++ data/risingwave/models/10_bronze/energy/battery_dc_converter_power_updates.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='materialized_view') }}
 
-WITH battery_dc_converter AS (
+WITH battery_dc_converter_data AS (
     SELECT * FROM {{ ref('battery_dc_converter') }}
 )
 
 {{ filter_marpower_topic_changes(
-    'battery_dc_converter', {
+    'battery_dc_converter_data', {
         'Voltage_A' : ('voltage_a', 'voltage_a_timestamp'),
         'Voltage_B' : ('voltage_b', 'voltage_b_timestamp'),
         'Current_A' : ('current_a', 'current_a_timestamp'),
--- data/risingwave/models/10_bronze/energy/bms_master_power_updates.sql
+++ data/risingwave/models/10_bronze/energy/bms_master_power_updates.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='materialized_view') }}
 
-WITH bms_master AS (
+WITH bms_master_data AS (
     SELECT * FROM {{ ref('bms_master') }}
 )
 
 {{ filter_marpower_topic_changes(
-    'bms_master', {
+    'bms_master_data', {
         'Stored_Energy' : ('stored_energy', 'stored_energy_timestamp')
     })
 }}
