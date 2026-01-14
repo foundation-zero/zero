42c5e1d190bbe7199853c6925ee2dbe113cd6d6b
Alje <alje.vandam@foundationzero.org>
2026-01-14 15:30:52 +0100

    Get loads app running with at stub

--- zero-loads-app/src/loads/cli.py
+++ zero-loads-app/src/loads/cli.py
@@ -66,7 +66,7 @@ class ControlCli(Settings):
 class SailSensorsStubCmd(GeneratorSettings):
     async def cli_cmd(self) -> None:
         logger.info("Running sail sensors stub...")
-        async with DataGenerator.init_from_settings(self) as data_gen:
+        async with DataGenerator.init_from_settings(self, 'sail_sensors_stub_generator') as data_gen:
             config = sail_systems.gen_config()
             await data_gen.generate(config=config)
 
@@ -74,7 +74,7 @@ class SailSensorsStubCmd(GeneratorSettings):
 class ATSensorsStubCmd(GeneratorSettings):
     async def cli_cmd(self) -> None:
         logger.info("Running A+T sensors stub...")
-        async with DataGenerator.init_from_settings(self) as data_gen:
+        async with DataGenerator.init_from_settings(self, 'at_sensors_stub_generator') as data_gen:
             config = at_systems.gen_config()
             await data_gen.generate(config=config)
 
@@ -93,8 +93,8 @@ class ZeroLoads(BaseSettings, cli_kebab_case=True):
     pcan_stub: CliSubCommand[PCanStubCmd]
     conditions_stub: CliSubCommand[ConditionsStubCmd]
     control: CliSubCommand[ControlCli]
-    sails_stub: CliSubCommand[SailSensorsStubCmd]
-    at_stub: CliSubCommand[ATSensorsStubCmd]
+    sail_sensors_stub: CliSubCommand[SailSensorsStubCmd]
+    at_sensors_stub: CliSubCommand[ATSensorsStubCmd]
 
     def cli_cmd(self) -> None:
         CliApp.run_subcommand(self)
