ecae7b9cd2c8e4dc2cfa68c89d2a9d3174a79946
Stefan Hofman <stefan.hofman@foundationzero.org>
2026-01-15 11:21:22 +0100

    Updated "metadata" macro to accept table ref
    Similar to the "updates" macro in a previous commit

--- data/risingwave/models/20_silver/energy/bms_master_power_data.sql
+++ data/risingwave/models/20_silver/energy/bms_master_power_data.sql
@@ -1,7 +1,3 @@
 {{ config(materialized='materialized_view') }}
 
-WITH bms_master_power_updates AS (
-    SELECT * FROM {{ ref('bms_master_power_updates') }}
-)
-
-{{ join_electrical_metadata('bms_master_power_updates') }}
+{{ join_electrical_metadata(ref('bms_master_power_updates')) }}
--- data/risingwave/models/20_silver/energy/consumer_producer_power_data.sql
+++ data/risingwave/models/20_silver/energy/consumer_producer_power_data.sql
@@ -1,7 +1,3 @@
 {{ config(materialized='materialized_view') }}
 
-WITH consumer_producer_power_updates AS (
-    SELECT * FROM {{ ref('consumer_producer_power_updates') }}
-)
-
-{{ join_electrical_metadata('consumer_producer_power_updates') }}
+{{ join_electrical_metadata(ref('consumer_producer_power_updates')) }}
