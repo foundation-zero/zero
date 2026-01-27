0450246ff6751e4a8156f45fa2fe4452fa4ba0cb
Francis <f.golinski@gmail.com>
2026-01-27 15:11:51 +0100

    Split cli into separate processes, add click

--- zero-thrs-control/src/thrs/cli/__main__.py
+++ zero-thrs-control/src/thrs/cli/__main__.py
@@ -1,11 +1,13 @@
-from argparse import ArgumentParser
 import logging
 import os
 import sys
 
+import asyncclick as click
+from asyncclick import BadParameter
+
 from thrs.cli.simulation_controls import (
-    MODES,
     SimulationControls,
+    MODES,
 )
 from thrs.orchestration.config import Config
 
@@ -21,31 +23,30 @@ def setup_logging():
 settings = Config()  # type: ignore
 
 
+@click.group
 async def main():
     setup_logging()
-    parser = ArgumentParser("THRS")
-
-    subparser = parser.add_subparsers()
-
-    run_cmd = subparser.add_parser("run", help="Run the THRS simulation and control")
-    run_cmd.add_argument(
-        "type",
-        choices=MODES.keys(),
-        help="Type of simulation to run",
-    )
-    run_cmd.set_defaults(func=run)
-
-    args = parser.parse_args()
-    if hasattr(args, "func"):
-        await args.func(args)
-    else:
-        parser.print_help()
 
 
-async def run(args):
+@main.command()
+async def simulate():
+    """Run simulation process for all the modules"""
+
+
+@main.command()
+async def control():
+    """Run the control modules"""
+
+
+@main.command()
+@click.argument("type")
+async def run(type):
+    """Run a simulation process for a single module"""
+    if type not in MODES:
+        raise BadParameter(f"TYPE must be one of {','.join(MODES.keys())}")
     async with SimulationControls.from_settings(settings) as controls:
         await controls.clear_previous()
-        await controls.run(args.type)
+        await controls.run(type)
 
 
 if __name__ == "__main__":
