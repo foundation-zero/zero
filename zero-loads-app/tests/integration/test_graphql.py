8427eae10f5f6c1bc0c051b43089a6816cc70a91
Alje <alje.vandam@foundationzero.org>
2026-02-05 23:50:38 +0100

    Add test

--- zero-loads-app/tests/integration/test_graphql.py
+++ zero-loads-app/tests/integration/test_graphql.py
@@ -98,6 +98,60 @@ async def test_graphql(async_client: AsyncClient, override_dependency):
 
 
 @pytest.mark.asyncio
+async def test_graphql_set_reference_values(
+    async_client: AsyncClient, override_dependency
+):
+    with override_dependency(get_messaging, override_messaging):
+        insert = await async_client.post(
+            "/graphql",
+            json={
+                "query": """
+                mutation {
+                    setReferenceValues(
+                        awaRanges: [upwind, reaching]
+                        awsRanges: [aws_0_10, aws_10_15]
+                        referenceValue: {id: "blade-adjuster-load", target: 100}
+                        sailSet: [full_main, full_mizzen]
+                    )
+                }
+                """
+            },
+        )
+
+        assert insert.status_code == 200
+
+        response = await async_client.post(
+            "/graphql",
+            json={
+                "query": """
+                query {
+                    variables(variables: ["blade-adjuster-load"]) {
+                        id
+                        reference(case: {awaRange: upwind, awsRange: aws_0_10, sailset: [full_main, full_mizzen]}) {
+                            target
+                        }
+                    }
+                }
+                """
+            },
+        )
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "data": {
+                "variables": [
+                    {
+                        "id": "blade-adjuster-load",
+                        "reference": {
+                            "target": 100.0,
+                        },
+                    }
+                ]
+            }
+        }
+
+
+@pytest.mark.asyncio
 async def test_graphql_all_variables(async_client: AsyncClient, override_dependency):
     with override_dependency(get_messaging, override_messaging):
         response = await async_client.post(
