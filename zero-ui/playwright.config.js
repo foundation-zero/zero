578fab71b1222c4a984e7274135c7a3f2b644704
David Hoevers <david@manklavier.nl>
2026-02-12 14:25:48 +0100

    ZERO-1396: use json instead of env in production

--- zero-ui/playwright.config.js
+++ zero-ui/playwright.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: "pnpm dev",
+    command: "pnpm dev -- --mode test",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
--- zero-ui/vitest.config.ts
+++ zero-ui/vitest.config.ts
@@ -1,25 +1,32 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  test: {
-    clearMocks: true,
-    globals: true,
-    environment: "jsdom",
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, "");
+  process.env = { ...process.env, ...env };
 
-  root: resolve(__dirname, "src"),
+  return {
+    plugins: [vue(), tailwindcss()],
+    test: {
+      clearMocks: true,
+      globals: true,
+      environment: "jsdom",
+    },
 
-  resolve: {
-    alias: [
-      { find: "@", replacement: resolve(__dirname, "src") },
-      { find: "@common", replacement: resolve(__dirname, "src/modules/common") },
-      { find: "@tests", replacement: resolve(__dirname, "tests") },
-      { find: "@components", replacement: resolve(__dirname, "src/components/ui") },
-      { find: "@modules", replacement: resolve(__dirname, "src/components/modules") },
-    ],
-  },
+    root: resolve(__dirname, "src"),
+
+    resolve: {
+      alias: [
+        { find: "@", replacement: resolve(__dirname, "src") },
+        { find: "@env", replacement: resolve(__dirname, "settings") },
+        { find: "@common", replacement: resolve(__dirname, "src/modules/common") },
+        { find: "@tests", replacement: resolve(__dirname, "tests") },
+        { find: "@components", replacement: resolve(__dirname, "src/components/ui") },
+        { find: "@modules", replacement: resolve(__dirname, "src/components/modules") },
+      ],
+    },
+  };
 });
