dee5d42921dd7135619010ef694507ec56b0a70c
Floris Smit <florissmit10@gmail.com>
2026-03-27 14:23:30 +0100

    Adapt to k8s side

--- vector/test.sh
+++ vector/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-actual_output="$(docker run -q --rm -w /home/vector -v "$(pwd):/home/vector" timberio/vector:0.54.0-debian --config tests/config.yaml -q -q)"
+actual_output="$(podman run -q --rm -w /home/vector -v "$(pwd):/home/vector" timberio/vector:0.54.0-debian --config tests/config.yaml -q -q)"
 
 if ! diff -u tests/expected.json <(printf '%s' "$actual_output") >/dev/null; then
   echo "Vector output does not match tests/expected.json, got output:" >&2
