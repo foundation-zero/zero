228cd761d035e478afd8435f028f5829d566e6eb
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2026-04-07 11:46:08 +0200

    Fix tests for IO lists (#277)

--- zero-thrs-control/src/thrs/input_output/modules/cooling_panels.py
+++ zero-thrs-control/src/thrs/input_output/modules/cooling_panels.py
@@ -7,6 +7,10 @@ class CoolingPanelsSensorValues(ThrsValues):
     cooling_pump_hydronic: Annotated[
         sensor.Pump, component_meta(yard_tag="50001037", component_type="pump")
     ]
+    cooling_temperature_fahrenheit_supply: Annotated[
+        sensor.TemperatureSensor,
+        component_meta(yard_tag="50001038-43", component_type="temperature_sensor"),
+    ]
     cooling_temperature_hydronic_return: Annotated[
         sensor.TemperatureSensor,
         component_meta(yard_tag="50001038-67", component_type="temperature_sensor"),
