d9dae610f1c1844c118f50d6da3e443afc2b59eb
Stefan Hofman <166694740+stefan-hofman@users.noreply.github.com>
2026-04-28 01:19:25 -0400

    Feature/Add yard tags to commissioning dashboards (#298)
    * Add mqtt data to metadata table
    
    * Add yard tags to bilge-fifi dashboard
    
    * Added yard tags to 2 data points on doors-hatches dashboard
    
    * Added yard tags to fresh water dashboard
    
    * Added yard tags to grey-black water dashboard
    
    * Added yard tags to Seawater dashboard
    
    * Added yard tags to Tank levels dashboard
    
    * Added yard tags to Technical water dashboard
    
    * Cleared default value for tag fields
    
    When no database connection, the tag fields are empty. When connection is available, the yard tags are displayed or a "-" is shown for fields where the yard tag is not present in the database.
    
    * Update test_io_metadata.py

--- zero-data/src/zero_data/dbt_gen/io_metadata.py
+++ zero-data/src/zero_data/dbt_gen/io_metadata.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from polars import DataFrame
 import logging
+import polars as pl
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +11,8 @@ class IOMetadataWriter:
     def __init__(self, dbt_path: Path):
         self.dbt_path = dbt_path.resolve()
         self.io_metadata_cols = [
+            "mqtt_topic",
+            "mqtt_json_path",
             "device",
             "tag",
             "yard_tag",
@@ -29,6 +32,12 @@ class IOMetadataWriter:
         metadata_path.parent.mkdir(parents=True, exist_ok=True)
 
         df = df.select(*self.io_metadata_cols)
+        df = df.with_columns(
+            pl.col("mqtt_json_path")
+            .str.replace(r"^\$\.", "")
+            .str.to_lowercase()
+            .alias("mqtt_json_path")
+        )
         df.write_csv(metadata_path, quote_style="non_numeric")
         logger.info(
             f"Wrote {df.shape[0]} rows and {df.shape[1]} columns to {metadata_path}"
