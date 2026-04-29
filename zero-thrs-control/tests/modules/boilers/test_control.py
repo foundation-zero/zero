c2e45f2a054d209ddd55fb1b6749ad7a50e7d203
Alje <alje.vandam@foundationzero.org>
2026-04-29 14:40:49 +0200

    Add tank level to fill test

--- zero-thrs-control/tests/modules/boilers/test_control.py
+++ zero-thrs-control/tests/modules/boilers/test_control.py
@@ -8,7 +8,15 @@ from thrs.orchestration.executor import SimulationExecutionResult
 
 
 async def test_filling(cycler: Cycler, simulation_inputs: BoilersSimulationInputs):
-    # TODO: set consumption to zero and check fill levels
+    simulation_inputs_no_consumption = simulation_inputs.model_copy(
+        update={
+            "boilers_freshwater_return_set": simulation_inputs.boilers_freshwater_return_set.model_copy(
+                update={"flow": Stamped.stamp(0)}
+            )
+        }
+    )
+    cycler.update_simulation_inputs(simulation_inputs_no_consumption)
+
     result = await cycler.run(60)
 
     assert isinstance(cycler._control, BoilersControl)
@@ -38,13 +46,8 @@ async def test_filling(cycler: Cycler, simulation_inputs: BoilersSimulationInput
     assert result.sensor_values.boilers_flow_lt1.flow.value == approx(0.0, abs=0.01)
     assert result.sensor_values.boilers_flow_lt2.flow.value > 0.1
 
-    cycler._control.update_parameters(
-        cycler._control.parameters.copy(
-            update={"maximum_tank_level": 10}
-        )  # all tanks full
-    )
-
-    result = await cycler.run(180)
+    # wait for tanks to fill
+    result = await cycler.run(1000)
     assert isinstance(result, SimulationExecutionResult)
     assert isinstance(cycler._control, BoilersControl) and isinstance(
         cycler._control._tanks_controller, TanksController
@@ -55,6 +58,18 @@ async def test_filling(cycler: Cycler, simulation_inputs: BoilersSimulationInput
 
     assert result.sensor_values.boilers_flow_lt1.flow.value == approx(0.0, abs=0.01)
     assert result.sensor_values.boilers_flow_lt2.flow.value == approx(0.0, abs=0.01)
+    assert (
+        result.sensor_values.boilers_level_tank1.level.value
+        > cycler._control.parameters.minimum_tank_level
+    )
+    assert (
+        result.sensor_values.boilers_level_tank2.level.value
+        > cycler._control.parameters.minimum_tank_level
+    )
+    assert (
+        result.sensor_values.boilers_level_tank3.level.value
+        > cycler._control.parameters.minimum_tank_level
+    )
 
 
 async def test_boosting_transitions(
