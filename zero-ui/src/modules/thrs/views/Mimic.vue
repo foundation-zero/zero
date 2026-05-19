0fbf7f356614d98f284a956c13755e961308d780
David Hoevers <david@manklavier.nl>
2026-05-19 11:51:39 +0200

    ZERO-1789: add boilers mimic to thrsim

---
+++ zero-ui/src/modules/thrs/views/Mimic.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BoilersModule from "@/modules/thrapp/mimics/modules/boilers/BoilersModule.vue";
+import { DEFINITIONS } from "@/modules/thrs/lib/consts";
+import { inject, Ref } from "vue";
+
+const currentDefinition = inject<Ref<keyof typeof DEFINITIONS>>("currentModule")!;
+</script>
+<template>
+  <BoilersModule v-if="currentDefinition === 'boilers'" />
+</template>
