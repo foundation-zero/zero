2043217bb93d04368d48701c36f43e32b7b644b8
David Hoevers <david@manklavier.nl>
2026-05-29 10:35:41 +0200

    Use static width/height to fix Safari bug

--- zero-ui/src/modules/thrapp/mimics/components/bypass-area/BypassArea.vue
+++ zero-ui/src/modules/thrapp/mimics/components/bypass-area/BypassArea.vue
@@ -11,7 +11,11 @@ defineProps<{ width: number; height: number }>();
       height="100%"
     >
       <div
-        class="border-flows-pipe text-2xs bg-background/60 text-muted-foreground h-full w-full flex-col rounded-md border border-dashed p-1 text-center"
+        class="border-flows-pipe text-2xs bg-background/60 text-muted-foreground flex-col rounded-md border border-dashed p-1 text-center"
+        :style="{
+          height: `${height}px`,
+          width: `${width}px`,
+        }"
       >
         <slot />
       </div>
