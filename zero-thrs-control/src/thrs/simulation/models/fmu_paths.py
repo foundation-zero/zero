e13ffcf920836707bd1d90bbe9bbc3c3e030168d
Alje <alje.vandam@foundationzero.org>
2026-06-16 15:56:14 +0200

    Update thrusters module

--- zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
+++ zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-thrusters_path = str(Path(__file__).resolve().parent / "thruster_moduleV18.fmu")
 pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV15_1.fmu")
 consumers_path = str(Path(__file__).resolve().parent / "Consumer_ModuleV8_2.fmu")
 pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV7.fmu")
@@ -10,7 +9,7 @@ boilers_path = str(Path(__file__).resolve().parent / "Boilers_ModuleV4_1.fmu")
 lt1_path = str(Path(__file__).resolve().parent / "LT1_ModuleV3_1.fmu")
 lt2_path = str(Path(__file__).resolve().parent / "LT2_ModuleV3_2.fmu")
 
-# thrusters_path = str(Path(__file__).resolve().parent / "Thrusters_ModuleV19.fmu")
+thrusters_path = str(Path(__file__).resolve().parent / "Thruster_Module_V19.fmu")
 # pvt_path = str(Path(__file__).resolve().parent / "PVT_ModuleV16.fmu")
 # consumers_path = str(Path(__file__).resolve().parent / "Consumer_ModuleV9.fmu")
 # pcm_path = str(Path(__file__).resolve().parent / "PCM_ModuleV8.fmu")
