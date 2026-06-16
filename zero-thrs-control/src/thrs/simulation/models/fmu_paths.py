c56399350a037474721b28abce037044f6ba8aa2
Alje <alje.vandam@foundationzero.org>
2026-06-16 16:02:04 +0200

    Update PVT module

--- zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
+++ zero-thrs-control/src/thrs/simulation/models/fmu_paths.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-pvt_path = str(Path(__file__).resolve().parent / "pvt_moduleV15_1.fmu")
 consumers_path = str(Path(__file__).resolve().parent / "Consumer_ModuleV8_2.fmu")
 pcm_path = str(Path(__file__).resolve().parent / "pcm_moduleV7.fmu")
 high_temperature_path = str(Path(__file__).resolve().parent / "HT_SystemV13_2.fmu")
@@ -10,7 +9,7 @@ lt1_path = str(Path(__file__).resolve().parent / "LT1_ModuleV3_1.fmu")
 lt2_path = str(Path(__file__).resolve().parent / "LT2_ModuleV3_2.fmu")
 
 thrusters_path = str(Path(__file__).resolve().parent / "Thruster_Module_V19.fmu")
-# pvt_path = str(Path(__file__).resolve().parent / "PVT_ModuleV16.fmu")
+pvt_path = str(Path(__file__).resolve().parent / "PVT_ModuleV16.fmu")
 # consumers_path = str(Path(__file__).resolve().parent / "Consumer_ModuleV9.fmu")
 # pcm_path = str(Path(__file__).resolve().parent / "PCM_ModuleV8.fmu")
 # high_temperature_path = str(Path(__file__).resolve().parent / "HT_SystemV14.fmu")
