2c6a62f952fb94de640287c68831e98b7bff1f98
Alje <alje.vandam@foundationzero.org>
2026-06-16 15:33:51 +0200

    Add Adsorption module

---
+++ zero-thrs-control/tests/modules/adsorption/conftest.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+
+from pytest import fixture
+
+from thrs.control.modules.adsorption import AdsorptionControl, AdsorptionParameters
+from thrs.input_output.base import Stamped
+from thrs.input_output.definitions.simulation import (
+    AdsorptionChiller,
+    Boundary,
+    TemperatureBoundary,
+)
+from thrs.input_output.modules.adsorption import (
+    AdsorptionSensorValues,
+    AdsorptionSimulationInputs,
+    AdsorptionSimulationOutputs,
+)
+from thrs.orchestration.simulation import Simulation
+from thrs.simulation.fmu import Fmu
+from thrs.simulation.models.fmu_paths import adsorption_path
+
+
+@fixture
+def simulation_inputs():
+    return AdsorptionSimulationInputs(
+        adsorption_cooling_supply=TemperatureBoundary(temperature=Stamped.stamp(20.0)),
+        adsorption_seawater_supply=Boundary(
+            temperature=Stamped.stamp(32.0), flow=Stamped.stamp(64.0)
+        ),
+        adsorption_available_cold_temperature=TemperatureBoundary(
+            temperature=Stamped.stamp(20.0)
+        ),
+        adsorption_available_hot_temperature=TemperatureBoundary(
+            temperature=Stamped.stamp(65.0)
+        ),
+        adsorption_available_seawater_temperature=TemperatureBoundary(
+            temperature=Stamped.stamp(30.0)
+        ),
+        adsorption_chiller=AdsorptionChiller(free_cooling=Stamped.stamp(False)),
+        adsorption_ht_supply=Boundary(
+            temperature=Stamped.stamp(60.0), flow=Stamped.stamp(42.0)
+        ),
+        adsorption_dhw_supply=Boundary(
+            temperature=Stamped.stamp(40.0), flow=Stamped.stamp(45.0)
+        ),
+    )
+
+
+@fixture
+def control(simulation):
+    return AdsorptionControl(AdsorptionParameters(), simulation.time)
+
+
+@fixture
+def simulation(simulation_inputs):
+    with Fmu(adsorption_path) as fmu:
+        yield Simulation(
+            AdsorptionSensorValues,
+            AdsorptionSimulationOutputs,
+            fmu,
+            simulation_inputs,
+            datetime.now(),
+            timedelta(seconds=1),
+        )
---
+++ zero-thrs-control/tests/modules/adsorption/test_simulation.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+import pytest
+from pytest import fixture
+
+from tests.helpers.simulation_inputs import simulator_input_field_setters
+from thrs.input_output.modules.adsorption import (
+    AdsorptionSensorValues,
+    AdsorptionSimulationInputs,
+    AdsorptionSimulationOutputs,
+)
+from thrs.orchestration.simulation import Simulation
+from thrs.simulation.fmu import Fmu
+from thrs.simulation.models.fmu_paths import fahrenheit_path
+
+
+@fixture(
+    params=list(simulator_input_field_setters(AdsorptionSimulationInputs, ignore=[]))
+)
+def incorrect_simulation_inputs(simulation_inputs, request):
+    inputs = simulation_inputs.get_values_at_time(datetime.now())
+    request.param(inputs, -9e7)
+    return inputs
+
+
+async def test_thrusters_simulation_inputs(incorrect_simulation_inputs, control):
+    with Fmu(fahrenheit_path) as fmu:
+        simulation = Simulation(
+            AdsorptionSensorValues,
+            AdsorptionSimulationOutputs,
+            fmu,
+            incorrect_simulation_inputs,
+            datetime.now(),
+            timedelta(seconds=1),
+        )
+
+        with pytest.raises(Exception):
+            for i in range(100):
+                await simulation.tick(
+                    control.initial(datetime.now()).values,
+                )
