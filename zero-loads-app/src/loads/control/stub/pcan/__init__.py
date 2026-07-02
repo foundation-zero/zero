be5cece78c81942a0834034abe5bdb30f9a61ff7
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2026-07-02 14:51:24 +0200

    Remove PCAN

--- zero-loads-app/src/loads/control/stub/pcan/__init__.py
+++
@@ -1,4 +0,0 @@
-from .can_frame import CAN_CRC_Frame, CAN_FD_CRC_Frame, CAN_FD_Frame, CAN_Frame
-from .pcan import PCanStub
-
-__all__ = ["PCanStub", "CAN_Frame", "CAN_FD_Frame", "CAN_CRC_Frame", "CAN_FD_CRC_Frame"]
