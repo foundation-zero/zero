5e66243b1cd2037032f5b6d0444ed8a778a6c163
Hmvp <hmvp@users.noreply.github.com>
2026-07-07 16:10:01 +0200

    Reduce duplication in runtime setup

--- zero-thrs-control/src/thrs/cli/cli.py
+++ zero-thrs-control/src/thrs/cli/cli.py
@@ -1,5 +1,8 @@
 import logging
+from datetime import datetime, timedelta
+from typing import Callable
 
+from aiomqtt import Client as MqttClient
 from pydantic_settings import (
     BaseSettings,
     CliApp,
@@ -7,40 +10,145 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
+from thrs.input_output.base import CombinedValues
+from thrs.orchestration.comms import (
+    ControlChannels,
+    DirectivesChannels,
+    MqttConnector,
+    SimulationChannels,
+)
 from thrs.orchestration.config import Config
 from thrs.orchestration.log import setup_logging
-from thrs.runtime.descriptions.simulation import Modes
+from thrs.orchestration.module import CombinedAlarms, CombinedControl
+from thrs.orchestration.simulation import Simulation
+from thrs.runtime.descriptions.simulation import MODES, Mode, Modes
+from thrs.runtime.directives import DirectiveHandling
+from thrs.runtime.runners.control import ControlRunner
+from thrs.runtime.runners.lockstep import LockstepRunner
+from thrs.runtime.runners.simulator import SimulationRunner
 from thrs.runtime.runtime import Runtime
 
 logger: logging.Logger = logging.getLogger(__name__)
 settings = Config()  # type: ignore
 
 
+def lookup_mode(mode: Modes) -> Mode:
+    return next((m for m in MODES if m.name == mode))
+
+
+def setup_simulation(
+    connector: MqttConnector, config: Config, mode: Mode
+) -> tuple[SimulationChannels, Simulation]:
+    simulation = mode.setup_simulation()
+    if simulation is None:
+        raise ValueError("simulation must be defined for simulation mode")
+
+    return (
+        SimulationChannels(
+            connector,
+            config,
+            mode.control_module.sensor_values_clss,
+            mode.control_module.control_values_clss,
+            simulation.inputs_cls,
+            simulation.outputs_cls,
+        ),
+        simulation,
+    )
+
+
+def setup_control(
+    connector: MqttConnector,
+    config: Config,
+    mode: Mode,
+    time_fn: Callable[[], datetime],
+) -> tuple[ControlChannels, CombinedControl, CombinedAlarms]:
+    control_channels = ControlChannels(connector, config, mode.control_module)
+
+    parameters = {
+        module: mode.control_module.parameters_for_module(module)()
+        for module in mode.control_module.modules
+    }
+
+    control = mode.control_module.control(CombinedValues(parameters), time_fn)
+
+    return control_channels, control, mode.control_module.alarms()
+
+
 class ControlCmd(Config):
-    mode: Modes
+    modes: Modes
 
     async def cli_cmd(self) -> None:
-        async with Runtime.setup_for_control(settings, self.mode) as runtime:
+        mode = lookup_mode(self.modes)
+        async with MqttClient(settings.mqtt_host, settings.mqtt_port) as mqtt_client:
+            connector = MqttConnector(mqtt_client)
+
+            runner_args = setup_control(connector, settings, mode, datetime.now)
+            runner = ControlRunner(*runner_args)
+            runtime = Runtime(runner, connector, timedelta(seconds=1))
+
             await runtime.loop.play(1)
             logger.info("Running control")
             await runtime.start()
 
 
 class SimulationCmd(Config):
-    mode: Modes
+    modes: Modes
 
     async def cli_cmd(self) -> None:
-        async with Runtime.setup_for_simulation(settings, self.mode) as runtime:
+        mode = lookup_mode(self.modes)
+
+        async with MqttClient(settings.mqtt_host, settings.mqtt_port) as mqtt_client:
+            connector = MqttConnector(mqtt_client=mqtt_client)
+
+            channels, simulation = setup_simulation(connector, settings, mode)
+            runner = SimulationRunner(channels, simulation)
+            runtime = Runtime(runner, connector, simulation.tick_duration)
+
             await runtime.loop.play(1)
             logger.info("Running simulation")
             await runtime.start()
 
 
 class LockstepCmd(Config):
-    mode: Modes
+    modes: Modes
 
     async def cli_cmd(self) -> None:
-        async with Runtime.setup_for_lockstep(settings, self.mode) as runtime:
+        mode = lookup_mode(self.modes)
+        async with (
+            MqttClient(settings.mqtt_host, settings.mqtt_port) as mqtt_client,
+        ):
+            connector = MqttConnector(mqtt_client)
+
+            simulation_channels, simulation = setup_simulation(
+                connector, settings, mode
+            )
+
+            control_channels, control, alarms = setup_control(
+                connector, settings, mode, simulation.time
+            )
+
+            runner = LockstepRunner(
+                control=control,
+                control_channels=control_channels,
+                simulation=simulation,
+                simulation_channels=simulation_channels,
+                alarms=alarms,
+            )
+
+            directives_channels = DirectivesChannels(connector, settings)
+
+            directive_handling = DirectiveHandling(
+                directives_channels,
+                mode,
+                simulation.time,
+            )
+            runtime = Runtime(
+                runner,
+                connector,
+                simulation.tick_duration,
+                directive_handling,
+            )
+
             await runtime.clear_previous()
             logger.info("Running lockstep")
             await runtime.start()
--- zero-thrs-control/src/thrs/runtime/runtime.py
+++ zero-thrs-control/src/thrs/runtime/runtime.py
@@ -1,171 +1,29 @@
 from asyncio import TaskGroup
-from contextlib import asynccontextmanager
-from datetime import datetime, timedelta
-from typing import AsyncGenerator, Coroutine
+from datetime import timedelta
 
-from aiomqtt import Client as MqttClient
-
-from thrs.input_output.base import CombinedValues
-from thrs.orchestration.comms import (
-    ControlChannels,
-    DirectivesChannels,
-    MqttConnector,
-    SimulationChannels,
-)
-from thrs.orchestration.config import Config
-from thrs.orchestration.module import CombinedControl
-from thrs.runtime.descriptions.simulation import MODES, Mode, Modes
+from thrs.orchestration.comms import MqttConnector
 from thrs.runtime.directives import DirectiveHandling
 from thrs.runtime.loop import EMPTY_HOOKS, Loop
 from thrs.runtime.runners.base import Runner
-from thrs.runtime.runners.control import ControlRunner
-from thrs.runtime.runners.lockstep import LockstepRunner
-from thrs.runtime.runners.simulator import SimulationRunner
 
 
 class Runtime:
     def __init__(
         self,
         runner: Runner,
+        connector: MqttConnector,
         tick_duration: timedelta,
         directive_handling: DirectiveHandling | None = None,
-        connector_runs: list[Coroutine[None, None, None]] | None = None,
     ):
         self._loop = Loop(tick_duration)
         self._runner = runner
         self._directive_handling = directive_handling
-        self._connectors = connector_runs or []
-
-    @staticmethod
-    def _lookup_mode(mode: Modes) -> Mode:
-        return next((m for m in MODES if m.name == mode))
-
-    @asynccontextmanager
-    @staticmethod
-    async def setup_for_simulation(
-        config: Config, selected_mode: Modes
-    ) -> "AsyncGenerator[Runtime, None]":
-        mode = Runtime._lookup_mode(selected_mode)
-        simulation = mode.setup_simulation()
-        if simulation is None:
-            raise ValueError("simulation must be defined for simulation mode")
-
-        async with MqttClient(config.mqtt_host, config.mqtt_port) as simulation_client:
-            connector = MqttConnector(mqtt_client=simulation_client)
-
-            channels = SimulationChannels(
-                connector,
-                config,
-                mode.control_module.sensor_values_clss,
-                mode.control_module.control_values_clss,
-                simulation.inputs_cls,
-                simulation.outputs_cls,
-            )
-            yield Runtime(
-                SimulationRunner(channels, simulation),
-                simulation.tick_duration,
-                connector_runs=[await connector.run()],
-            )
-
-    @asynccontextmanager
-    @staticmethod
-    async def setup_for_lockstep(
-        config: Config, selected_mode: Modes
-    ) -> "AsyncGenerator[Runtime, None]":
-        mode = Runtime._lookup_mode(selected_mode)
-        simulation = mode.setup_simulation()
-        if simulation is None:
-            raise ValueError("simulation must be defined for lockstep mode")
-        async with (
-            MqttClient(config.mqtt_host, config.mqtt_port) as directive_client,
-            MqttClient(config.mqtt_host, config.mqtt_port) as simulation_client,
-            MqttClient(config.mqtt_host, config.mqtt_port) as control_client,
-        ):
-            control_connector = MqttConnector(control_client)
-            control_channels = ControlChannels(
-                control_connector,
-                config,
-                mode.control_module,
-            )
-
-            parameters = {
-                module: mode.control_module.parameters_for_module(module)()
-                for module in mode.control_module.modules
-            }
-
-            control: CombinedControl = mode.control_module.control(
-                CombinedValues(parameters), simulation.time
-            )
-
-            simulation_connector = MqttConnector(simulation_client)
-            simulation_channels = SimulationChannels(
-                simulation_connector,
-                config,
-                mode.control_module.sensor_values_clss,
-                mode.control_module.control_values_clss,
-                simulation.inputs_cls,
-                simulation.outputs_cls,
-            )
-            runner = LockstepRunner(
-                control=control,
-                control_channels=control_channels,
-                simulation=simulation,
-                simulation_channels=simulation_channels,
-                alarms=mode.control_module.alarms(),
-            )
-
-            directive_connector = MqttConnector(directive_client)
-
-            directives_channels = DirectivesChannels(directive_connector, config)
-
-            directive_handling = DirectiveHandling(
-                directives_channels,
-                mode,
-                simulation.time,
-            )
-            yield Runtime(
-                runner,
-                simulation.tick_duration,
-                directive_handling,
-                connector_runs=[
-                    await control_connector.run(),
-                    await simulation_connector.run(),
-                    await directive_connector.run(),
-                ],
-            )
-
-    @asynccontextmanager
-    @staticmethod
-    async def setup_for_control(
-        config: Config, selected_mode: Modes
-    ) -> "AsyncGenerator[Runtime, None]":
-        mode = Runtime._lookup_mode(selected_mode)
-        async with MqttClient(config.mqtt_host, config.mqtt_port) as control_client:
-            connector = MqttConnector(control_client)
-            control_channels = ControlChannels(connector, config, mode.control_module)
-
-            parameters = {
-                module: mode.control_module.parameters_for_module(module)()
-                for module in mode.control_module.modules
-            }
-
-            control = mode.control_module.control(
-                CombinedValues(parameters), datetime.now
-            )
-            runner = ControlRunner(
-                channels=control_channels,
-                control=control,
-                alarms=mode.control_module.alarms(),
-            )
-            yield Runtime(
-                runner, timedelta(seconds=1), connector_runs=[await connector.run()]
-            )
+        self._connector = connector
 
     async def start(self):
         """Start the runtime, including the loop and any directive handling if present. Hooks are used to send status messages for directive handling."""
         async with TaskGroup() as tg:
-            for connector in self._connectors:
-                tg.create_task(connector)
+            tg.create_task(await self._connector.run())
 
             if self._directive_handling is not None:
                 await self._directive_handling.handler(self._loop).register()
