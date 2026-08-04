d55cf8987f270b441cb06319b0d418a36370861d
Hmvp <hmvp@users.noreply.github.com>
2026-08-04 16:32:13 +0200

    Fix pvt snake casing

--- zero-thrs-control/src/thrs/input_output/modules/pvt.py
+++ zero-thrs-control/src/thrs/input_output/modules/pvt.py
@@ -1,7 +1,7 @@
+import re
 from typing import Annotated
 
 from pydantic import ConfigDict, computed_field
-from pydantic.alias_generators import to_snake
 
 from thrs.input_output.base import (
     ThrsValues,
@@ -9,6 +9,30 @@ from thrs.input_output.base import (
     computed_meta,
 )
 from thrs.input_output.definitions import control, sensor, simulation
+
+
+def to_snake(camel: str) -> str:
+    """Convert a PascalCase, camelCase, or kebab-case string to snake_case.
+
+    Args:
+        camel: The string to convert.
+
+    Returns:
+        The converted string in snake_case.
+    """
+    # Handle the sequence of uppercase letters followed by a lowercase letter
+    snake = re.sub(
+        r"([A-Z]+)([A-Z][a-z])", lambda m: f"{m.group(1)}_{m.group(2)}", camel
+    )
+    # Insert an underscore between a lowercase letter and an uppercase letter
+    snake = re.sub(r"([a-z])([A-Z])", lambda m: f"{m.group(1)}_{m.group(2)}", snake)
+    # Insert an underscore between a digit and an uppercase letter
+    snake = re.sub(r"([0-9])([A-Z])", lambda m: f"{m.group(1)}_{m.group(2)}", snake)
+    # Insert an underscore between a lowercase letter and a digit
+    # snake = re.sub(r'([a-z])([0-9])', lambda m: f'{m.group(1)}_{m.group(2)}', snake)
+    # Replace hyphens with underscores to handle kebab-case
+    snake = snake.replace("-", "_")
+    return snake.lower()
 
 
 class PvtSensorValues(ThrsValues):
