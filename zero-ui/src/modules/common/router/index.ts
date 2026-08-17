b447fa4b538f65ac6c0375a461aea4145a2a185e
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2026-08-17 13:41:39 +0200

    Add sail system HMI to UI

--- zero-ui/src/modules/common/router/index.ts
+++ zero-ui/src/modules/common/router/index.ts
@@ -20,4 +20,14 @@ export const grafanaRoute: RouteRecordRaw = {
   component: () => import("@/modules/common/views/Grafana.vue"),
 };
 
+export const sailSystemRoute: RouteRecordRaw = {
+  path: "/sail-system",
+  name: "sail-system",
+  meta: {
+    requiresAuth: false,
+    layout: defineAsyncComponent(() => import("../layouts/EmptyLayout.vue")),
+  },
+  component: () => import("@/modules/common/views/SailSystem.vue"),
+};
+
 export const getRootRoute = (route: string = "") => route.split(":")[0];
