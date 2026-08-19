faf6fe209ba4bebc950a8334ad4f851f5c3bae33
Hmvp <hmvp@users.noreply.github.com>
2026-08-19 11:52:12 +0200

    Display valve absolute position from backend data

--- zero-ui/src/modules/thrapp/mimics/tooltips/partials/ValvePosition.vue
+++ zero-ui/src/modules/thrapp/mimics/tooltips/partials/ValvePosition.vue
@@ -3,7 +3,6 @@ import { SensorComponentType } from "@/modules/thrsim/types";
 import * as Partials from ".";
 import { useTranslations } from "..";
 import { ModuleField, SensorValue } from "../../providers";
-import { FieldRenderer } from "../../renderers";
 const { items } = useTranslations();
 
 defineProps<{ valve: ModuleField<SensorComponentType.Valve> }>();
@@ -17,10 +16,12 @@ defineProps<{ valve: ModuleField<SensorComponentType.Valve> }>();
     <Partials.ListItem no-source>
       {{ items("relativePosition") }}
     </Partials.ListItem>
-    <Partials.ListItem
-      no-source
-      :renderer="FieldRenderer.PositionAbsolute"
-    >
+  </SensorValue>
+  <SensorValue
+    :source="valve"
+    field="positionAbs"
+  >
+    <Partials.ListItem no-source>
       {{ items("absolutePosition") }}
     </Partials.ListItem>
   </SensorValue>
