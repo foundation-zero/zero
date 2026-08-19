d2a052e172756de7082676f14450e30e06c57006
Hmvp <hmvp@users.noreply.github.com>
2026-08-19 11:52:35 +0200

    Add debug logging for incomplete SensorValues

--- zero-thrs-control/src/thrs/input_output/model_builder.py
+++ zero-thrs-control/src/thrs/input_output/model_builder.py
@@ -1,10 +1,13 @@
+import logging
 from abc import ABC, abstractmethod
 from asyncio import Future
 from typing import Any
 
-from pydantic import TypeAdapter
+from pydantic import TypeAdapter, ValidationError
 
 from thrs.input_output.base import ThrsValues
+
+logger = logging.getLogger(__name__)
 
 
 class ModelBuilder[T](ABC):
@@ -55,6 +58,15 @@ class PartialModelBuilder[T: ThrsValues](ModelBuilder[T]):
                 self._complete_model.set_result(self._value)
 
     def result(self) -> T | None:
+        if logger.isEnabledFor(logging.DEBUG):
+            try:
+                self._cls(**self._values)
+            except ValidationError as e:
+                logger.debug(
+                    "Failed creating model",
+                    exc_info=e,
+                )
+
         return self._value
 
     async def wait_for_result(self) -> T:
