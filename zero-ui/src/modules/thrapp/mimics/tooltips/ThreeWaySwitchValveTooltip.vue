eec83a586c70556f6084083c702fae0e783bc30e
Hmvp <hmvp@users.noreply.github.com>
2026-09-08 12:20:53 +0200

    Change graph field for valves

--- zero-ui/src/modules/thrapp/mimics/tooltips/ThreeWaySwitchValveTooltip.vue
+++ zero-ui/src/modules/thrapp/mimics/tooltips/ThreeWaySwitchValveTooltip.vue
@@ -36,7 +36,7 @@ const { labels, items, sources } = useTranslations();
       <SensorGraph
         :device="tooltip?.technicalName"
         type="valves"
-        field="cc_setpoint"
+        field="position_rel"
       />
       <ManualControl />
     </TooltipList>
