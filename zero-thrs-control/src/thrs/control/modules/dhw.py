43a96d76b2b6c7ed6d25b3e71a9905f327ed6de4
Alje <alje.vandam@foundationzero.org>
2026-09-09 08:31:58 +0200

    Fix last dutypoint

--- zero-thrs-control/src/thrs/control/modules/dhw.py
+++ zero-thrs-control/src/thrs/control/modules/dhw.py
@@ -152,6 +152,10 @@ def _INITIAL_CONTROL_VALUES(timestamp: datetime) -> DhwControlValues:  # noqa: N
 
 
 def _INITIAL_CONTROLLER_STATE(timestamp: datetime) -> DhwControllerState:  # noqa: N802
+
+    pump_pid_zero = PidController.zero(timestamp)
+    pump_pid_zero.setpoint = Stamped(value=0.1, timestamp=timestamp)
+
     return DhwControllerState(
         dhw_tanks_controller=TanksControllerValues(
             tank1_state=Stamped(value=TankState.NEEDS_FILL, timestamp=timestamp),
@@ -161,8 +165,8 @@ def _INITIAL_CONTROLLER_STATE(timestamp: datetime) -> DhwControllerState:  # noq
         ),
         dhw_drives_flow_controller=PidController.zero(timestamp),
         dhw_dc_flow_controller=PidController.zero(timestamp),
-        dhw_pump_flow_controller=PidController.zero(timestamp),
-        dhw_pump_temperature_controller=PidController.zero(timestamp),
+        dhw_pump_flow_controller=pump_pid_zero,
+        dhw_pump_temperature_controller=pump_pid_zero,
     )
 
 
